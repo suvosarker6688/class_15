424eaee9467e307dac02b50b81f045f6be2f9daf
suvosarker6688 <126939552+suvosarker6688@users.noreply.github.com>
2023-03-13 22:35:39 +0600

    add price range

--- conditional.php
+++ conditional.php
@@ -1,8 +1,27 @@
 <?php
-$name = "mithun";
-print $name;
+/*Conditionals
+if 
+elseif
+else
+while
+switch
+*/
+
+$price = 44;
+ 
+if($price<25){
+    print('বিস্কুটের দাম ২৫ টাকা।');
+}elseif($price>40 && $price<50){
+        print("আইস্ক্রিমের দাম ৪১-৫০ টাকা ।");
+    }elseif($price>50 && $price<80){
+        print("চকলেটের দাম ৫০-৮০ টাকা ।");
+    }elseif ($price>80) {
+      print("বারগারের দাম ৮০-১০০ টাকা ।");
+    }else {
+        print("something went wrong");
+    }
 
 
-$bikes = ["discovery","bajaj","palsure"];
-print_r($bikes[1])
+
+ 
 ?>
